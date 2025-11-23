c02128c6b20de33e25f209e97d0f8d45dc932414
Shara Hysén <shara.hysen@outlook.com>
2025-11-23 12:52:34 +0100

    Added a new function in HealtAnalyzer, summery_stats

--- health_analysis.py
+++ health_analysis.py
@@ -7,10 +7,12 @@ class HealtAnalyzer:
         self.df = df
 
     def info_print(self):
+        """Visar olika typer av info från dataframe (head, info, describe, duplicates)"""
         display(self.df.head())
         display(self.df.info())
         display(self.df.describe())
         print('Dubblettrader: ',self.df.duplicated().any())
     
     def summery_stats(self, columns):
+        """Räknar medelvärde, median, min och max av valda kolumner"""
         return self.df[columns].agg(['mean', 'median', 'min', 'max']).round(2)
